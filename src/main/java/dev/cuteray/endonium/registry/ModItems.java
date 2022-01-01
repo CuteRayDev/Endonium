30fd245e9d290eb8474427d1a28bff2abb9a99b0
CuteRayDev <cuteraydev@gmail.com>
2022-01-01 17:49:07 +0800

    Added data and some textures to 4 new items

--- src/main/java/dev/cuteray/endonium/registry/ModItems.java
+++ src/main/java/dev/cuteray/endonium/registry/ModItems.java
@@ -12,9 +12,17 @@ import static dev.cuteray.endonium.Endonium.MOD_PREFIX;
 public class ModItems {
 
     public static Item ENDONIUM = new Item(new Item.Settings().group(ItemGroup.MATERIALS).fireproof());
+    public static Item ENITE = new Item(new Item.Settings().group(ItemGroup.MATERIALS));
+    public static Item MELTEDDIAMOND = new Item(new Item.Settings().group(ItemGroup.MATERIALS));
+    public static Item MELTEDENITE = new Item(new Item.Settings().group(ItemGroup.MATERIALS));
+    public static Item ENDORY = new Item(new Item.Settings().group(ItemGroup.MATERIALS));
 
     public static void RegisterItems(){
         Registry.register(Registry.ITEM, new Identifier(Endonium.MOD_ID, "endonium"), ENDONIUM);
+        Registry.register(Registry.ITEM, new Identifier(Endonium.MOD_ID, "enite"), ENITE);
+        Registry.register(Registry.ITEM, new Identifier(Endonium.MOD_ID, "melteddiamond"), MELTEDDIAMOND);
+        Registry.register(Registry.ITEM, new Identifier(Endonium.MOD_ID, "meltedenite"), MELTEDENITE);
+        Registry.register(Registry.ITEM, new Identifier(Endonium.MOD_ID, "endory"), ENDORY);
         System.out.println(MOD_PREFIX + " Items Loaded.");
     }
 }
